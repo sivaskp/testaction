0cf2ff9bf84b3cd1b9e15898bb9b0f724150b3ad
sivakanthpuvvadi <sivakanth.puvvadi@aa.com>
2022-12-29 03:59:23 -0500

    added script file

---
+++ .github/workflows/script.sh
@@ -0,0 +1,23 @@
+echo 1. what is the directory
+ls -la 
+echo
+echo 2. Is java installed
+java -version
+echo
+echo 3. Is git installed
+git --version
+echo
+echo 4. build tools
+mvn --version
+ant -version
+gradle --version
+echo 
+echo 5. who is running the script
+whoami
+echo 
+echo 6. disk layout
+df -h
+echo
+echo 7. env variables
+env
+echo
